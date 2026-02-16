ebacda5c83d03ce7924834216f22cfb3d0fb9ecb
Robin Scherrer <mail@robinscherrer.com>
2026-02-16 19:09:40 +0100

    chore(release): bump version to v0.1.0

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ type convertConfig struct {
 const (
 	cliName           = "cfop-generator"
 	zonefileParseExp  = `^(\S+)\s+(\d+)\s+IN\s+([A-Z]+)\s+(.+)$`
-	version           = "dev"
+	version           = "v0.1.0"
 	dnsrecordTemplate = `
 {{- range . }}
 ---
